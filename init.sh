d9790d78265e5530deb0d115ea4c3fe47992f2c3
aartaka <aartaka@protonmail.com>
2020-05-31 08:23:42 +0500

    Delete init.sh

--- init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir ./lisp
-ln -sf . ~/.emacs.d
-curl https://raw.githubusercontent.com/tuhdo/emacs-c-ide-demo/master/custom/setup-helm.el > emacs/lisp/setup-helm.el
-curl https://raw.githubusercontent.com/arclanguage/anarki/master/extras/inferior-arc.el > emacs/lisp/inferior-arc.el
-curl https://raw.githubusercontent.com/arclanguage/anarki/master/extras/arc.el > emacs/lisp/arc.el
-curl https://www.emacswiki.org/emacs/download/column-marker.el > emacs/lisp/column-marker.el
-curl https://raw.githubusercontent.com/pmarinov/clean-aindent-mode/master/clean-aindent-mode.el > emacs/lisp/clean-aindent-mode.el
