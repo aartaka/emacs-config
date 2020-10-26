29422207a517135422dea4d665413b541697ddcc
aartaka <aartaka@protonmail.com>
2020-10-27 01:21:11 +0500

    custom: Move Custom autogenerated code out of init file.

--- init.el
+++ init.el
@@ -721,22 +721,9 @@
   (load-theme 'monochrome t)
   (load-theme 'aartaka-monochrome t))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
- '(package-selected-packages
-   '(pinentry powerline synosaurus wordnut auto-revert exwm-firefox-core exwm md4rd ox-gfm bbdb pretty-sha-path miniedit web-mode yasnippet-classic-snippets yasnippet-snippets yasnippet-lean skewer-less skewer rainbow-mode keyfreq all-the-icons sly nov pdf-tools esup elisp--witness--lisp flymake-racket racket-mode use-package w3 autopair org-mode))
- '(send-mail-function 'smtpmail-send-it))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
 (put 'narrow-to-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
