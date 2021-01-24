1d7d7b28a93bb286a91253f6a9e50604e3d6618d
Artyom Bologov <aartaka@protonmail.com>
2021-01-24 23:07:20 +0500

    install: Load packages in lisp/ subdir.

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 mkdir lisp
-ln -sf . ~/.emacs.d
-curl https://raw.githubusercontent.com/tuhdo/emacs-c-ide-demo/master/custom/setup-helm.el > emacs/lisp/setup-helm.el
-curl https://raw.githubusercontent.com/arclanguage/anarki/master/extras/inferior-arc.el > emacs/lisp/inferior-arc.el
-curl https://raw.githubusercontent.com/arclanguage/anarki/master/extras/arc.el > emacs/lisp/arc.el
-curl https://www.emacswiki.org/emacs/download/column-marker.el > emacs/lisp/column-marker.el
-curl https://raw.githubusercontent.com/pmarinov/clean-aindent-mode/master/clean-aindent-mode.el > emacs/lisp/clean-aindent-mode.el
+curl https://www.emacswiki.org/emacs/download/screenshot.el > lisp/screenshot.el
+curl https://raw.githubusercontent.com/tuhdo/emacs-c-ide-demo/master/custom/setup-helm.el > lisp/setup-helm.el
+curl https://raw.githubusercontent.com/arclanguage/anarki/master/extras/inferior-arc.el > lisp/inferior-arc.el
+curl https://raw.githubusercontent.com/arclanguage/anarki/master/extras/arc.el > lisp/arc.el
+curl https://www.emacswiki.org/emacs/download/column-marker.el > lisp/column-marker.el
+curl https://raw.githubusercontent.com/pmarinov/clean-aindent-mode/master/clean-aindent-mode.el > lisp/clean-aindent-mode.el
