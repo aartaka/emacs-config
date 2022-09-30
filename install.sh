a56810c53415ca13fe5641ec237e13eb2a9fe6f3
Artyom Bologov <aartaka@protonmail.com>
2022-09-30 16:52:28 +0400

    install: Remove meaningless/unused downloads lines.

--- install.sh
+++ install.sh
@@ -5,5 +5,3 @@ curl https://www.emacswiki.org/emacs/download/screenshot.el > lisp/screenshot.el
 curl https://raw.githubusercontent.com/tuhdo/emacs-c-ide-demo/master/custom/setup-helm.el > lisp/setup-helm.el
 curl https://raw.githubusercontent.com/arclanguage/anarki/master/extras/inferior-arc.el > lisp/inferior-arc.el
 curl https://raw.githubusercontent.com/arclanguage/anarki/master/extras/arc.el > lisp/arc.el
-curl https://www.emacswiki.org/emacs/download/column-marker.el > lisp/column-marker.el
-curl https://raw.githubusercontent.com/pmarinov/clean-aindent-mode/master/clean-aindent-mode.el > lisp/clean-aindent-mode.el
